c994796901cf92433152f43c30d1196b5dd31a84
Krishna Prabhu <krishnaprabhu21@gmail.com>
2016-01-19 13:20:40 +0530

    changed user view based on sign in status

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   get 'welcome/index'
+
+  devise_for :users
+
+  resources :notes
+
+  authenticated :user do
+    root 'root#index', as: "authenticated_root"
+  end
 
   root 'welcome#index'
 
-  resources :notes
 end
